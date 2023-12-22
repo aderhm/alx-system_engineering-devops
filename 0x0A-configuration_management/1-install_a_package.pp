8ded6d1f44ab3e2b827da80dc685c5cb37e53c1d
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-12-22 22:20:00 +0100

    Install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
-# Install flask from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Install flask from pip3i
+class install_flask {
+
+  package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
+  }
 }
+
+include install_flask
