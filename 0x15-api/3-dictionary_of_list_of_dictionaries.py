fc136d1428004b1c2e234db8c831d1381f1c7baf
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-01-29 22:23:30 +0100

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,41 @@
 #!/usr/bin/python3
+"""This script returns information about a TODO list
+of a all users.
+"""
+
+import json
+import requests
+
+
+def get_all_tasks():
+    user_response = requests.get(
+        'https://jsonplaceholder.typicode.com/users'
+        ).json()
+
+    todos_response = requests.get(
+        'https://jsonplaceholder.typicode.com/todos'
+        ).json()
+
+    users = {}
+    for user in user_response:
+        user_id = user.get('id')
+        user_name = user.get('username')
+        todo_data = list(
+            filter(lambda x: x.get('userId') == user_id, todos_response)
+            )
+        user_data = list(map(
+                lambda x: {
+                    "username": user_name,
+                    "task": x.get("title"),
+                    "completed": x.get("completed")
+                },
+                todo_data
+            ))
+        users['{}'.format(user_id)] = user_data
+
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(users, file)
+
+
+if __name__ == '__main__':
+    get_all_tasks()
