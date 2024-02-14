eeb587c9b560824fe9d6e165f83ced5087dc7a7c
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-02-14 22:37:18 +0100

    Fix php extensions

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixes bad `php` extensions
 exec {'fix-extension':
-	command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-	path => "/usr/local/bin/:/bin/"
+	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+	path => '/usr/local/bin/:/bin/'
 
 }
