4a69af75e33d4303ebbac0722844700fefd2d825
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-02-23 00:26:19 +0100

    bring the limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,9 +3,8 @@
 exec { 'increase-traffic':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
-} ->
-
-exec { 'restart':
+}
+-> exec { 'restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
 }
